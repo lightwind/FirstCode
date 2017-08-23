a7256e9f1ea856625cb10be1f6b353fd096efcf2
light <747962518@qq.com>
2017-08-23 18:33:37 +0800

    自定义内容提供器

---
+++ 17-ContactsTest/app/src/main/java/com/lightwind/a17_contactstest/MyProvider.java
@@ -0,0 +1,97 @@
+package com.lightwind.a17_contactstest;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * 功能：自定义内容提供器
+ * 作者：刘洋
+ * 时间：2017/8/23
+ */
+
+public class MyProvider extends ContentProvider {
+
+    private static final int TABLE1_DIR = 0;
+    private static final int TABLE1_ITEM = 1;
+    private static final int TABLE2_DIR = 2;
+    private static final int TABLE2_ITEM = 3;
+
+    private static UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.app.provider", "table1", TABLE1_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table1/#", TABLE1_ITEM);
+        uriMatcher.addURI("com.example.app.provider", "table1", TABLE2_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table1/#", TABLE2_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String
+            selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                // 查询table1中所有的数据
+                break;
+            case TABLE1_ITEM:
+                // 查询table1中的单条数据
+                break;
+            case TABLE2_DIR:
+                // 查询table2中所有的数据
+                break;
+            case TABLE2_ITEM:
+                // 查询table2中的单条数据
+                break;
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table1";
+            case TABLE1_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table1";
+            case TABLE2_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table2";
+            case TABLE2_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table2";
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[]
+            selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String
+            selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
