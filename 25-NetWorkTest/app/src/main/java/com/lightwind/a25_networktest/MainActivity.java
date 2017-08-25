51f1a0df73f988c4153d326f1a4b0d53cdc1924d
light <747962518@qq.com>
2017-08-25 10:23:37 +0800

    WebView基本使用

---
+++ 25-NetWorkTest/app/src/main/java/com/lightwind/a25_networktest/MainActivity.java
@@ -0,0 +1,14 @@
+
+package com.lightwind.a25_networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
